950d4c2080ef018366b37d2448b86142855e1831
Bilalsikandarkhatik <bilalkhatik870@gmail.com>
2025-05-26 16:38:01 +0530

    Create Allowance_Wallet.sol

---
+++ Allowance_Wallet.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AllowanceWallet {
+    address public owner;
+
+    mapping(address => uint256) public allowance;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this.");
+        _;
+    }
+
+    function setAllowance(address _user, uint256 _amount) public onlyOwner {
+        allowance[_user] = _amount;
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(allowance[msg.sender] >= _amount, "Not enough allowance.");
+        require(address(this).balance >= _amount, "Contract has insufficient balance.");
+
+        allowance[msg.sender] -= _amount;
+        payable(msg.sender).transfer(_amount);
+    }
+
+    function deposit() public payable onlyOwner {}
+
+    receive() external payable {}
+}
